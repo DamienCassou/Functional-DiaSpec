bb42ff404ae3ac8240f9f800cd1ac0ea3b419a6a
Damien Cassou <damien.cassou@gmail.com>
2010-05-12 19:22:42 +0200

    Adds a sample file to contain a sample framework and its implementation

---
+++ testFramework.hs
@@ -0,0 +1,15 @@
+module Main where
+
+-- Example generated framework
+
+-- context C1 as Boolean {
+--   <pull self; ; push self>
+-- }
+
+-- Java version:
+--  abstract class AbstractC1 { Boolean get(); }
+
+data AbstractC1 = AbstractC1 {get :: Bool -> Bool}
+
+
+-- Example implementation
