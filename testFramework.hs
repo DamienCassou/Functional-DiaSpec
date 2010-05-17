df75f3de2fd56a46998df835ddbff623f488c157
Damien Cassou <damien.cassou@gmail.com>
2010-05-17 19:53:57 +0200

    Add some more examples

--- testFramework.hs
+++ testFramework.hs
@@ -8,13 +8,7 @@ module Main where
 --  abstract class AbstractC1 { Boolean get(); }
 
 -- Haskell version:
-data AbstractC1 = AbstractC1 {get :: () -> Bool}
-
--- Example implementation
-C1 {
-  get = true
-  }
-  
+data AbstractC1 = AbstractC1 {abstractC1_get :: () -> Bool}
 
 -- context C2 as Boolean {  
 -- <pull self; pull C1 ; push self>
@@ -22,6 +16,30 @@ C1 {
 
 -- Boolean get(C1Closure c1);
 
-data AbstractC2 = AbstractC2 {get :: (() -> Boolean) -> 
+data AbstractC2 = AbstractC2 {abstractC2_get :: (() -> Bool) -> Bool}
+
+-- example implementation which negates the value returned by c1. Is
+-- it possible to make that simpler? By merging the definitions?
+
+myabstractC2_get :: (() -> Bool) -> Bool
+myabstractC2_get c1Closure = not (c1Closure ())
+
+myC2 = AbstractC2 myabstractC2_get
+
+-- context C1Indexed as Boolean indexed by val as Integer {
+--   <pull self; ; push self>
+-- }
+
+-- Boolean get(Integer val)
+
+data AbstractC1Indexed = AbstractC1Indexed {abstractC1Indexed_get :: Integer -> Bool}
+
+-- context C2Indexed as Boolean indexed by val as Integer {
+--   <pull self; pull C1Indexed; push self>
+-- }
+
+-- Boolean get(C1Closure c1)
+
+data AbstractC2Indexed = AbstractC2Indexed {abstractC2Indexed_get :: Integer -> (Integer -> Bool) -> Bool}
 
 
